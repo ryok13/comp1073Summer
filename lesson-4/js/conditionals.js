8e0606ed08fa1417f649acab3ca641ade7d5ed27
ryok13 <mario24ryok@gmail.com>
2025-05-27 10:21:14 -0400

    week4 part1

--- lesson-4/js/conditionals.js
+++ lesson-4/js/conditionals.js
@@ -2,8 +2,17 @@ const output = document.querySelector('body p:nth-of-type(2)');
 		
 /* STEP 1a: A simple if/else statement */
 let homeWorkDone = null;
+let breakDuration = 0;
 
-// STEP 1b: Any value that is not false, undefined, null, 0, NaN, or an empty string will evaluate to TRUE when tested using a conditional statement - try a few other values for var homeWorkDone and test
+if (homeWorkDone === false) {
+	breakDuration = 10;
+} else {
+	breakDuration = 60;
+}
+
+output.textContent = `I get ${breakDuration} minutes for a break.`;
+
+// STEP 1b: Any value (that is not false, undefined, null, 0, NaN, or an empty string) will evaluate to TRUE when tested using a conditional statement - try a few other values for var homeWorkDone and test
 
 /* STEP 2: The more complex if|elseif|else */
 const page = document.querySelector('html');
@@ -14,11 +23,12 @@ const weatherButton = document.querySelector('#weatherButton');
 const comments = document.querySelector('#commentary');
 
 /* STEP 2a: Add an event listener for the 'click' event on the 'Set Weather' button that invokes the setWeather() function below */
-
+weatherButton.addEventListener("click", setWeather); // Do not parentheses
 
 function setWeather() {
 	let choice = select.value;
 	let temperature = temp.value;
+	console.log(`The weather is ${choice}, and the temperature is ${temperature}.`);
 	/* STEP 2b: Craft an IF/ELSEIF/ELSE that changes the src attribute of the icon element to the appropriate .svg file in the images folder */
 
 	/* STEP 3: Add a nested IF/ELSE statement inside the 'sunny' condition above that tests for temperature, and if it is equal to or greater than 15, turn the page background orange, otherwise turn it lightblue */
